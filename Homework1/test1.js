b54a1af3702362366cf90c46f1545dd1d37637a5
dkuznetsov21 <Dmytro.Kuznetsov@innovecs.com>
2019-11-20 11:59:30 +0200

    home work 1,2

---
+++ Homework1/test1.js
@@ -0,0 +1,18 @@
+/** 1 task */
+let fruit = `ba${0/0}a`;
+console.log(fruit);
+
+/** 2 task */
+let breakfest = fruit;
+
+/** 3 task */
+const amount = 3.14, currency = "USD";
+
+/** 4 task */
+let first = 5;
+let second = 7;
+let fruits = first ** second;
+console.log(fruits);
+
+/** 5 task */
+let firstWords = "Hello world!";
