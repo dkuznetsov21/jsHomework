d6ec8b5833dfd8ab2b1fb9c6df97e78746bff028
dkuznetsov21 <Dmytro.Kuznetsov@innovecs.com>
2019-11-20 13:05:04 +0200

    home work 4

--- Homework4/test.js
+++ Homework4/test.js
@@ -1,4 +1,4 @@
-//  -----------  Issue 1.
+//Issue 1.
 const dog = new Object();
 console.log(dog);
 dog.name = "Moochskctar";
@@ -13,7 +13,7 @@ delete dog.age;
 console.log(dog);
 
 
-// ------------ Issue2
+//Issue2
 function isEmpty(obj) {
     return Object.keys(obj) == 0;
 }
@@ -23,11 +23,11 @@ const QTY = {
     Dogs: 100000,
     Cats: 1625476540,
     Bananas: 155330
-};
+}
 let allAnimals = QTY.Dogs + QTY.Cats;
 console.log(allAnimals);
 
-//---------  Issue 4
+//Issue 4
 function mumu(obj) {
     const obj1 = {};
     for (let key in obj) {
@@ -42,40 +42,39 @@ function mumu(obj) {
 testClass = {a: 4, b: 5, c: "as"};
 console.log(mumu(testClass));
 
-// ------------ Issue 5
-function getName() {
-    for (let prop in this) {
-        if (prop === 'name') {
-            return this[prop];
-        } else {
-            return 'The object has no property Name'
-        }
+//Issue 5
+function returnName() {
+    return this.name
+}
+obj1 = { age : 23,name:"Devid"};
+obj2 = {name: 'John' , live : true};
+obj1.returnName = returnName;
+console.log(obj1.returnName());
+obj2.returnName = returnName;
+console.log(obj2.returnName());
+
+}
+
+
+//Issue 6
+export class Controller {
+    constructor(finished = false, done = 5, atempts = 44) {
+        this.finished = finished;
+        this.done = done;
+        this.atempts = atempts;
+    }
+
+    returnAtempts() {
+        return this.atempts;
+    }
+
+    changeFinish() {
+        this.finished = !this.finished;
     }
 }
-dog.getName = getName;
-QTY.getName = getName;
-console.log('Result Task 5: Object with the name property. DOG name is :', dog.getName());
-console.log('Result Task 5: Object without the name property. QTY name is : ', QTY.getName());
 
-
-//-----------Issue 6.
-
-class Controller {
-    constructor() {
-        this.finished = false;
-        this.done = 5;
-        this.atempts = 44;
-        this.getAtempts = function() {
-            return this.atempts;
-        };
-        this.switchFinished = function() {
-            if (this.finished===true){
-                this.finished = false;
-            }else{
-                this.finished = true;
-            }
-        }
-
-    }
-}
-module.exports = Controller;
+let amazing = new Controller();
+amazing.changeFinish();
+let a = amazing.returnAtempts();
+console.log(a);
+console.log(amazing.finished);
